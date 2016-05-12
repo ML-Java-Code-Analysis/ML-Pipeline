f330b07d34176241d76020858b66c5eea581b9da
Yacine Mekesser <ymekesser@hotmail.com>
2016-05-12 23:21:24 +0200

    Ein paar mehr Tests

--- test/test_models.py
+++ test/test_models.py
@@ -1,15 +1,13 @@
 import unittest
 
 import test_datasets
-from sklearn import linear_model
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing.data import StandardScaler
+import numpy as np
 
 from ml import Model, Predict, Reporting
 
 
 class ModelTestCase(unittest.TestCase):
-    def _test_dataset(self, model, train_dataset, test_dataset):
+    def _test_dataset(self, model, train_dataset, test_dataset, precision=7):
         model.fit(train_dataset.data, train_dataset.target)
         train_prediction = Predict.predict_with_model(train_dataset, model)
         test_prediction = Predict.predict_with_model(test_dataset, model)
@@ -21,10 +19,10 @@ class ModelTestCase(unittest.TestCase):
             [Reporting.SCORE_R2S, Reporting.SCORE_MAE, Reporting.SCORE_MDE, Reporting.SCORE_EVS])
         print(comparisation_table.table)
 
-        self.assertAlmostEqual(train_report.r2s, 1.0)
-        self.assertAlmostEqual(train_report.mae, 0.0)
-        self.assertAlmostEqual(test_report.r2s, 1.0)
-        self.assertAlmostEqual(test_report.mae, 0.0)
+        self.assertAlmostEqual(train_report.r2s, 1.0, places=precision)
+        self.assertAlmostEqual(train_report.mae, 0.0, places=precision)
+        self.assertAlmostEqual(test_report.r2s, 1.0, places=precision)
+        self.assertAlmostEqual(test_report.mae, 0.0, places=precision)
 
 
 class TestLinearRegression(ModelTestCase):
@@ -35,7 +33,7 @@ class TestLinearRegression(ModelTestCase):
         train_dataset, test_dataset = test_datasets.get_simple_linear_datasets()
         self._test_dataset(model, train_dataset, test_dataset)
 
-    def test_simple_linear_dataset_normalized(self):
+    def test_simple_linear_dataset_scaled(self):
         model = Model.create_model(
             model_type=Model.MODEL_TYPE_LINREG,
             feature_scaling=True
@@ -61,23 +59,69 @@ class TestLinearRegression(ModelTestCase):
         self._test_dataset(model, train_dataset, test_dataset)
 
 
-
 class TestRidgeRegression(ModelTestCase):
     def test_simple_linear_dataset(self):
-        pass
+        model = Model.create_model(
+            model_type=Model.MODEL_TYPE_RIDREG,
+            alpha=0
+        )
+        train_dataset, test_dataset = test_datasets.get_simple_linear_datasets()
+        self._test_dataset(model, train_dataset, test_dataset)
 
-    def test_simple_linear_dataset_normalized(self):
-        pass
+    def test_simple_linear_dataset_scaled(self):
+        model = Model.create_model(
+            model_type=Model.MODEL_TYPE_RIDREG,
+            feature_scaling=True,
+            alpha=0
+        )
+        train_dataset, test_dataset = test_datasets.get_simple_linear_datasets()
+        self._test_dataset(model, train_dataset, test_dataset)
 
-    def test_simple_linear_dataset_cross_validation(self):
-        pass
+    def test_simple_linear_dataset_poly(self):
+        model = Model.create_model(
+            model_type=Model.MODEL_TYPE_RIDREG,
+            polynomial_degree=2,
+            alpha=0
+        )
+        train_dataset, test_dataset = test_datasets.get_simple_linear_datasets()
+        self._test_dataset(model, train_dataset, test_dataset)
+
+    def test_simple_linear_dataset_full_pipeline(self):
+        model = Model.create_model(
+            model_type=Model.MODEL_TYPE_RIDREG,
+            polynomial_degree=2,
+            feature_scaling=True,
+            alpha=0.5
+        )
+        train_dataset, test_dataset = test_datasets.get_simple_linear_datasets()
+        self._test_dataset(model, train_dataset, test_dataset, precision=0)
+
+
+class TestRidgeRegressionCV(ModelTestCase):
+    def test_simple_linear_dataset(self):
+        model = Model.create_model(
+            model_type=Model.MODEL_TYPE_RIDREG,
+            alpha_range=[0, 0.01, 0.1, 1, 10, 100, 1000]
+        )
+        train_dataset, test_dataset = test_datasets.get_simple_linear_datasets()
+        self._test_dataset(model, train_dataset, test_dataset, 2)
+
+    def test_simple_linear_dataset_full_pipeline(self):
+        model = Model.create_model(
+            model_type=Model.MODEL_TYPE_RIDREG,
+            feature_scaling=False,
+            polynomial_degree=1,
+            alpha_range=[0, 0.01, 0.1, 1, 10, 100, 1000]
+        )
+        train_dataset, test_dataset = test_datasets.get_simple_linear_datasets()
+        self._test_dataset(model, train_dataset, test_dataset, 0)
 
 
 class TestSVR(ModelTestCase):
     def test_simple_linear_dataset(self):
         pass
 
-    def test_simple_linear_dataset_normalized(self):
+    def test_simple_linear_dataset_scaled(self):
         pass
 
     def test_simple_linear_dataset_cross_validation(self):

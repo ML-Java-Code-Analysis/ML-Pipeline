5e404d7182c8e22aa7e575347a9b7b3ce45e57da
Yacine Mekesser <ymekesser@hotmail.com>
2016-05-14 16:15:20 +0200

    Testfälle mit polynomialen Datensets

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -91,3 +91,95 @@ def get_simple_linear_test_dataset():
     ])
 
     return dataset
+
+
+def get_simple_polynomial_datasets():
+    # return get_simple_polynomial_train_dataset(), get_simple_polynomial_test_dataset()
+    return get_simple_polynomial_dataset("Train", sample_size=100), get_simple_polynomial_dataset("Test")
+
+
+def get_simple_polynomial_dataset(label, sample_size=10):
+    dataset = Dataset(
+        5,
+        10,
+        ["F1, F2, F3, F4, F5"],
+        "sixmonth_bugs",
+        datetime.date(2015, 1, 1),
+        datetime.date(2015, 1, 31),
+        False,
+        label
+    )
+
+    dataset.data = np.random.rand(sample_size, 5)
+
+    dataset.target = np.zeros((sample_size, 1))
+    for i, row in enumerate(dataset.data):
+        dataset.target[i] = 8 + 3 * row[0] + row[1] + row[2] ** 2 + row[2] * row[4]
+
+    return dataset
+
+
+def get_simple_polynomial_train_dataset():
+    dataset = Dataset(
+        5,
+        10,
+        ["F1, F2, F3, F4, F5"],
+        "sixmonth_bugs",
+        datetime.date(2015, 1, 1),
+        datetime.date(2015, 1, 31),
+        False,
+        "SimpleLinearTrain"
+    )
+
+    dataset.data = np.array([
+        [0.0, 1.0, 5.0, 2.0, 0.0],
+        [0.0, 1.0, 5.0, 3.0, 0.0],
+        [0.0, 1.0, 5.0, 1.0, 0.0],
+        [0.0, 1.0, 5.0, 6.0, 0.0],
+        [0.0, 1.0, 5.0, 2.0, 0.0],
+        [10.0, 1.0, 25.0, 7.0, -5.0],
+        [10.0, 1.0, 25.0, 2.0, -5.0],
+        [10.0, 1.0, 25.0, 3.0, -5.0],
+        [20.0, 1.0, 6.0, 1.0, -10.0],
+        [50.0, 1.0, 6.0, 7.0, -25.0],
+    ])
+
+    dataset.target = np.zeros((10, 1))
+
+    for i, row in enumerate(dataset.data):
+        dataset.target[i] = 3 + row[0] + row[2] ** 2 + row[2] * row[4]
+
+    return dataset
+
+
+def get_simple_polynomial_test_dataset():
+    dataset = Dataset(
+        5,
+        10,
+        ["F1, F2, F3, F4, F5"],
+        "sixmonth_bugs",
+        datetime.date(2015, 1, 1),
+        datetime.date(2015, 1, 31),
+        False,
+        "SimpleLinearTrain"
+    )
+
+    dataset.data = np.array([
+        [10.0, 1.0, 5.0, 5.0, 0.0],
+        [20.0, 1.0, 5.0, 3.0, 0.0],
+        [30.0, 1.0, 5.0, 2.0, 1.0],
+        [40.0, 1.0, 255.0, 2.0, 0.0],
+        [50.0, 1.0, 5.0, 3.0, 0.0],
+        [60.0, 1.0, 2.0, 67.0, -5.0],
+        [70.0, 1.0, 23.0, 8.0, -1.0],
+        [80.0, 1.0, 21.0, 3.0, -5.0],
+        [90.0, 1.0, 6.0, 2.0, -20.0],
+        [100.0, 1.0, 5.0, 6.0, -25.0],
+    ])
+
+    dataset.target = np.zeros((10, 1))
+
+    for i, row in enumerate(dataset.data):
+        dataset.target[i] = 3 + row[0] + row[2] ** 2 + row[2] * row[4]
+
+    return dataset
--- test/test_models.py
+++ test/test_models.py
@@ -1,3 +1,4 @@
+import inspect
 import unittest
 
 import test_datasets
@@ -7,7 +8,7 @@ from ml import Model, Predict, Reporting
 
 
 class ModelTestCase(unittest.TestCase):
-    def _test_dataset(self, model, train_dataset, test_dataset, precision=7):
+    def _test_dataset(self, model, train_dataset, test_dataset, precision=7, title=None):
         model.fit(train_dataset.data, train_dataset.target)
         train_prediction = Predict.predict_with_model(train_dataset, model)
         test_prediction = Predict.predict_with_model(test_dataset, model)
@@ -17,7 +18,16 @@ class ModelTestCase(unittest.TestCase):
         comparisation_table = Reporting.get_report_comparisation_table(
             [train_report, test_report],
             [Reporting.SCORE_R2S, Reporting.SCORE_MAE, Reporting.SCORE_MDE, Reporting.SCORE_EVS])
+
+        if title:
+            print("\n" + "="*3 + " " + title + " " + "="*3)
         print(comparisation_table.table)
+
+        for step in model.steps:
+            try:
+                print("Coefs: " + str(step[1].coef_))
+            except:
+                pass
 
         self.assertAlmostEqual(train_report.r2s, 1.0, places=precision)
         self.assertAlmostEqual(train_report.mae, 0.0, places=precision)
@@ -57,6 +67,15 @@ class TestLinearRegression(ModelTestCase):
         )
         train_dataset, test_dataset = test_datasets.get_simple_linear_datasets()
         self._test_dataset(model, train_dataset, test_dataset)
+
+    def test_simple_polynomial_dataset_poly(self):
+        model = Model.create_model(
+            model_type=Model.MODEL_TYPE_LINREG,
+            feature_scaling=False,
+            polynomial_degree=2,
+        )
+        train_dataset, test_dataset = test_datasets.get_simple_polynomial_datasets()
+        self._test_dataset(model, train_dataset, test_dataset, title=str(type(self)) + "." + inspect.stack()[0][3])
 
 
 class TestRidgeRegression(ModelTestCase):
@@ -101,20 +120,32 @@ class TestRidgeRegressionCV(ModelTestCase):
     def test_simple_linear_dataset(self):
         model = Model.create_model(
             model_type=Model.MODEL_TYPE_RIDREG,
-            alpha_range=[0, 0.01, 0.1, 1, 10, 100, 1000]
+            alpha=0
         )
         train_dataset, test_dataset = test_datasets.get_simple_linear_datasets()
-        self._test_dataset(model, train_dataset, test_dataset, 2)
+        self._test_dataset(model, train_dataset, test_dataset)
 
     def test_simple_linear_dataset_full_pipeline(self):
         model = Model.create_model(
             model_type=Model.MODEL_TYPE_RIDREG,
-            feature_scaling=False,
+            cross_validation=True,
+            feature_scaling=True,
             polynomial_degree=1,
-            alpha_range=[0, 0.01, 0.1, 1, 10, 100, 1000]
+            alpha_range=[0, 0.1, 1, 10, 100]
         )
         train_dataset, test_dataset = test_datasets.get_simple_linear_datasets()
-        self._test_dataset(model, train_dataset, test_dataset, 0)
+        self._test_dataset(model, train_dataset, test_dataset)
+
+    def test_simple_polynomial_dataset_full_pipeline(self):
+        model = Model.create_model(
+            model_type=Model.MODEL_TYPE_RIDREG,
+            cross_validation=True,
+            feature_scaling=True,
+            polynomial_degree=2,
+            alpha_range=[0, 0.01, 0.1, 1, 10, 100, 1000, 10000]
+        )
+        train_dataset, test_dataset = test_datasets.get_simple_polynomial_datasets()
+        self._test_dataset(model, train_dataset, test_dataset, title=str(type(self)) + "." + inspect.stack()[0][3])
 
 
 class TestSVR(ModelTestCase):
